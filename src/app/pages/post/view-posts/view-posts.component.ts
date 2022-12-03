3a6c250b907207adbee2085036a9a10328dc916e
sinapy <sinasarsharpour@cmail.carleton.ca>
2022-12-02 22:54:16 -0500

    Update #2

--- src/app/pages/post/view-posts/view-posts.component.ts
+++ src/app/pages/post/view-posts/view-posts.component.ts
@@ -13,11 +13,11 @@ export class ViewPostsComponent implements OnInit {
   posts?: Post[];
   currentPost: Post = {};
   currentIndex = -1;
-  
+
 
   constructor(private postService: PostService) { }
 
-  
+
   ngOnInit(): void {
     this.retrievePosts();
 
@@ -28,12 +28,15 @@ export class ViewPostsComponent implements OnInit {
       .subscribe({
         next: (data) => {
           let postsList : Post[] = []
-
+          console.log(postsList);
           data.forEach(e => {
             e.expires_on = format((new Date(e.expires_on || "")),'yyyy-MM-dd')
             postsList.push(e)
           })
+          console.log("HELLO THIS IS HAPPENING")
+          console.log(postsList);
           this.posts = postsList;
+
           console.log(data);
         },
         error: (e) => console.error(e)
